08c235a86a68b010796d9e19f73a2d8fb49eab2a
PalmerinGD <diegopalmerin2002@gmail.com>
2022-02-28 13:16:21 -0600

    Styles of class person finished

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,9 +1,14 @@
+import './../class/person.css'
 const Person = ({person}) => {
     return (
-        <div className="">
-            <img src={person.image} alt={person.name}/>
-            <div>{person.name}</div>
-            <div>{person.age}</div>
+        <div className="profile-container">
+            <div className='profile-picture'>
+                <img src={person.image} alt={person.name}/>
+            </div>
+            <div className='profile-data'>
+                <div className='profile-name'>{person.name}</div>
+                <div className='profile-age'>{person.age}</div>
+            </div>
         </div>
     )
 }
